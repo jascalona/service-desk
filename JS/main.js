b764ef43c4f869c6a09bc6a29dd563263472d8c7
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-05-23 13:20:41 -0400

    new

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,23 @@
+
+const getOptionChart1=()=>{
+
+};
+
+const getOptionChart2=()=>{
+    
+};
+
+const initCharts=()=>{
+    const chart1=echarts.init(document.getElementById("chart1"));
+    const chart2=echarts.init(document.getElementById("chart2"));
+
+
+    chart1.setOption();
+    chart2.setOption();
+
+
+;}
+
 window.addEventListener('load', ()=> {
     console.log("ok");
 
