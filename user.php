9e913bcd05a59a6e832cfe10feb985bb13b7599c
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-05-28 09:15:59 -0400

    new

--- user.php
+++ user.php
@@ -67,11 +67,12 @@
             <table class="table">
                     <thead>
                         <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                        </tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">NAME</th>
+                        <th scope="col">SURNAME</th>
+                        <th scope="col">USER</th>
+                        <th scope="col">EMAIL</th>    
+                    </tr>
                     </thead>
                     <tbody>
                         <tr>
